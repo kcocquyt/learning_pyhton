9e337fb7c2ed59409aaa56c611d01386ed6b7a87
cocquyt koen <kcocquyt@volvocars.com>
2024-12-05 14:49:21 +0100

    python_easy_deel1

---
+++ Python_made_easy1/CH03/dic2.py
@@ -0,0 +1,8 @@
+capitalCities = {'UK': 'London',
+                 'Australia': 'Canberra',
+                 'USA': 'Washington DC',
+                 'Spain': 'Madrid'}
+
+USCapital = capitalCities ['Spain']
+
+print (USCapital)
---
+++ Python_made_easy1/Ch05/scope.py
@@ -0,0 +1,18 @@
+global_variable = 10
+local_variable = 5
+
+def my_function():
+    local_variable = 20
+    print(global_variable)  # Access global variable
+    print(local_variable)   # Access local variable
+
+my_function()
+print(global_variable)      # Access global variable
+print(local_variable)       # Error: Local variable  not accessible outside my_function
+
+
+
+
+
+
+
